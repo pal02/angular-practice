26f4d296c96948bc3210f7e5919de8ebd1e39240
pal02 <sindhwanipallavi121@gmail.com>
2020-06-19 07:57:00 +0530

    new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { practice2 } from './header/myheader.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
+import { HomeComponent } from './body/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    practice2,
+    FooterComponent,
+    BodyComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
