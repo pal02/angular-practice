a02b5cc200c5c7b51f15f5532065198eba390b50
pal02 <sindhwanipallavi121@gmail.com>
2020-06-21 16:30:59 +0530

    header-footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { practice2 } from './header/myheader.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
 import { HomeComponent } from './body/home/home.component';
+import { AboutComponent } from './body/about/about.component';
+import { ServiceComponent } from './body/service/service.component';
+import { BlogComponent } from './body/blog/blog.component';
+import { ContactComponent } from './body/contact/contact.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    practice2,
     FooterComponent,
     BodyComponent,
     HomeComponent,
+    AboutComponent,
+    ServiceComponent,
+    BlogComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
